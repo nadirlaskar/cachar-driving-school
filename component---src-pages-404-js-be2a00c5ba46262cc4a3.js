8624cb163b3487775a66ac35a6539c81b0ff518c
Nadir Hussain Laskar <nadirlaskar@gmail.com>
2024-01-13 16:16:59 +0000

    Updates

---
+++ component---src-pages-404-js-be2a00c5ba46262cc4a3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{6810:function(t,e,n){n.r(e);var u=n(7294),a=n(9267),l=n(4251);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-be2a00c5ba46262cc4a3.js.map
