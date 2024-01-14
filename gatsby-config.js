2c7f4c734db6158417fbb2d0df743543faab71de
Nadir Hussain Laskar <nadirlaskar@gmail.com>
2024-01-14 07:10:03 +0000

    added GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,9 +31,33 @@ module.exports = {
         display: `minimal-ui`,
         icon: `static/logo.png` // This path is relative to the root of the site.
       }
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: process.env.YOUR_GOOGLE_TAGMANAGER_ID,
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        gtmAuth: process.env.YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING,
+        gtmPreview: process.env.YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME,
+        dataLayerName: "dataLayer",
+        // Defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ]
 }
